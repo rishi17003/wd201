154ef00d427bf0ac0eb324459f013dc6656109b1
rishi17003 <102285269+rishi17003@users.noreply.github.com>
2023-12-14 22:38:16 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -7,66 +7,49 @@ const port = args.port || 3000;
 
 let homeContent = "";
 let projectContent = "";
-let regsitrationContent = "";
+let registrationContent = "";
+
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err;
   }
   homeContent = home;
-});
 
-fs.readFile("project.html", (err, project) => {
-  if (err) {
-    throw err;
-  }
-  projectContent = project;
-});
 
-fs.readFile("registration.html",(err,registration) => {
-  if (err) {
-    throw err;
-  }
-  regsitrationContent = registration;
-});
+  fs.readFile("project.html", (err, project) => {
+    if (err) {
+      throw err;
+    }
+    projectContent = project;
 
-// http
-//   .createServer((request, response) => {
-//     let url = request.url;
-//     response.writeHeader(200, { "Content-Type": "text/html" });
-//     switch (url) {
-//       case "/project":
-//         response.write(projectContent);
-//         response.end();
-//         break;
-//       case "/registration":
-//         response.write(regsitrationContent);
-//         response.end();
-//         break;
-//       default:
-//         response.write(homeContent);
-//         response.end();
-//         break;
-//     }
-//   })
-// .listen(3000);
+ 
+    fs.readFile("registration.html", (err, registration) => {
+      if (err) {
+        throw err;
+      }
+      registrationContent = registration;
 
-const server = http.createServer((request, response) => {
-  let url = request.url;
-  response.writeHead(200, { "Content-Type": "text/html" });
-  switch (url) {
-    case "/project":
-      response.write(projectContent);
-      break;
-    case "/registration":
-      response.write(registrationContent);
-      break;
-    default:
-      response.write(homeContent);
-      break;
-  }
-  response.end();
-});
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+      const server = http.createServer((request, response) => {
+        let url = request.url;
+        response.writeHead(200, { "Content-Type": "text/html" });
+        switch (url) {
+          case "/project":
+            response.write(projectContent);
+            break;
+          case "/registration":
+            response.write(registrationContent);
+            break;
+          default:
+            response.write(homeContent);
+            break;
+        }
+        response.end();
+      });
+
+      server.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+      });
+    });
+  });
 });
